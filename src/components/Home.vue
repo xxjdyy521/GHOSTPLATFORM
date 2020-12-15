7665265a8e2a28dcfc40f3aae7b4c3bd366afc1d
辛旭甲 <xinxujia@dhcc.com.cn>
2020-12-15 10:50:38 +0800

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <a-layout>
+  </a-layout>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data(){
+      return{
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
